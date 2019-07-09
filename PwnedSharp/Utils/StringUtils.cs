ab96c469269621a9bc174f89ddf4dd604ee630de
UpsettingBoy <jeronimosg@hotmail.es>
2019-07-10 00:09:18 +0200

    [WIP] Implementing password strength meter
    Password meter based on http://www.passwordmeter.com/
    
    - [WIP] Useful string operations on StringUtils
    - [WIP] PasswordMeter class

---
+++ PwnedSharp/Utils/StringUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PwnedSharp.Utils
+{
+    public static class StringUtils
+    {
+        private const ushort FIRST_UPPER = 0x0041;
+        private const ushort LAST_UPPER = 0x005A;
+
+        private const ushort FIRST_LOWER = 0x0061;
+        private const ushort LAST_LOWER = 0x007A;
+
+        private const ushort FIRST_NUMBER = 0x0030;
+        private const ushort LAST_NUMBER = 0x0039;
+
+        public static int CountUpperCaseLetters(this string value)
+        {
+            return value.Count(c => c >= FIRST_UPPER && c <= LAST_UPPER);
+        }
+
+        public static int CountLowerCaseLetters(this string value)
+        {
+            return value.Count(c => c >= FIRST_LOWER && c <= LAST_LOWER);
+        }
+
+        public static int CountNumbers(this string value)
+        {
+            return value.Count(c => c >= FIRST_NUMBER && c <= LAST_NUMBER);
+        }
+
+        public static int CountSymbols(this string value)
+        {
+            return value.Count(c => (c < FIRST_NUMBER || c > LAST_NUMBER)
+                                    && (c < FIRST_UPPER || c > LAST_UPPER)
+                                    && (c < FIRST_LOWER || c > LAST_LOWER));
+        }
+
+        public static bool ContainsOnlyLetters(this string value)
+        {
+            return value.All(c => (c >= FIRST_UPPER && c <= LAST_UPPER)
+                                  || (c >= FIRST_LOWER && c <= LAST_LOWER));
+        }
+
+        public static bool ContainsOnlyNumbers(this string value)
+        {
+            return value.CountNumbers() == value.Length;
+        }
+    }
+}
